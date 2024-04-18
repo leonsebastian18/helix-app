27728ea64e1c9298854bb29e8eaf556e34a7deea
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-04-18 17:46:17 -0500

    se consumieron initialProps y se manejaron los estilos desde el servidor

--- src/app/pages/Galaxies.tsx
+++ src/app/pages/Galaxies.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { GalaxiesLayout } from '../components/GalaxiesLayout';
 import { LoadingSpinner } from '../components/LoadingSpinner';
-import { LOADING_STATUS, ERROR_STATUS, useFetchGalaxiesInfo } from '../hooks/useFetchGalaxiesInfo';
+
 
 const GalaxiesPageWrapper = styled.div`
   display: flex;
@@ -11,14 +11,13 @@ const GalaxiesPageWrapper = styled.div`
   width: calc(100% - 6rem);
 `
 
-export const Galaxies = () => {
+export const Galaxies = (props) => {
 
-  const { galaxies, requestStatus } = useFetchGalaxiesInfo()
 
   const renderLayout = () => {
-    if (requestStatus === LOADING_STATUS) return <LoadingSpinner />
-    if (requestStatus === ERROR_STATUS) return <h1>Error</h1>
-    return <GalaxiesLayout galaxies={galaxies} />
+    if (props.galaxies.length === 0) return <LoadingSpinner />
+    
+    return <GalaxiesLayout galaxies={props.galaxies} />
   }
 
   return (
@@ -27,4 +26,8 @@ export const Galaxies = () => {
       {renderLayout()}
     </GalaxiesPageWrapper>
   );
+}
+
+Galaxies.defaultProps = {
+  galaxies: []
 }
